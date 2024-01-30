82082587b6311a38447e5f1c8af27c488fefb933
BurcuHaddad <burcuhaddad@gmail.com>
2024-01-30 22:09:13 +0300

    entities

---
+++ cruddemo/src/main/java/com/devbh/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,14 @@
+package com.devbh.cruddemo.dao;
+
+import com.devbh.cruddemo.entity.Instructor;
+
+public class AppDAOImpl implements AppDao{
+
+    // define field for entity manager
+
+    // inject entity manager using constructor injection
+    @Override
+    public void save(Instructor theInstructor) {
+
+    }
+}
---
+++ cruddemo/src/main/java/com/devbh/cruddemo/dao/AppDao.java
@@ -0,0 +1,7 @@
+package com.devbh.cruddemo.dao;
+
+import com.devbh.cruddemo.entity.Instructor;
+
+public interface AppDao {
+    void save(Instructor theInstructor);
+}
