36cec5d7dc4e837274b90597363d1bbd85ad7ee5
BurcuHaddad <burcuhaddad@gmail.com>
2024-02-03 14:46:27 +0300

    add instructor

--- cruddemo/src/main/java/com/devbh/cruddemo/CruddemoApplication.java
+++ cruddemo/src/main/java/com/devbh/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.devbh.cruddemo;
 
+import com.devbh.cruddemo.dao.AppDao;
+import com.devbh.cruddemo.entity.Instructor;
+import com.devbh.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +16,37 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(AppDao appDao) {
 		return runner -> {
-			System.out.println("Hello World");
+			createInstructor(appDao);
 		};
+	}
+
+	private void createInstructor(AppDao appDao) {
+		/*// create the instructor
+		Instructor tempInstructor =
+				new Instructor("Burcu", "Haddad", "burcu@bhdev.com");
+
+		// create the instructor detail
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail("http://www.burcudev.com/youtube", "Coding");*/
+
+		Instructor tempInstructor =
+				new Instructor("Madhu", "Patel", "madhu@bhdev.com");
+		InstructorDetail tempInstructorDetail =
+				new InstructorDetail("http://www.burcudev.com/youtube", "Guitar");
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		//save the instructor
+		// NOTE: this will also save the details object
+		// because of CascadeType.ALL
+
+		System.out.println("Saving instructor: " + tempInstructor);
+		appDao.save(tempInstructor);
+
+		System.out.println("Done !");
 	}
 
 }
--- cruddemo/src/main/java/com/devbh/cruddemo/dao/AppDAOImpl.java
+++ cruddemo/src/main/java/com/devbh/cruddemo/dao/AppDAOImpl.java
@@ -4,7 +4,9 @@ import com.devbh.cruddemo.entity.Instructor;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class AppDAOImpl implements AppDao{
 
     // define field for entity manager
